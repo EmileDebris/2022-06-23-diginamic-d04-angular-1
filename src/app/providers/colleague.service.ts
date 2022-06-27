cb7926d194d9c6fbc00c3599783d0a18a8844a80
Emile DEBRIS <emile.gd@orange.fr>
2022-06-27 14:02:17 +0200

    mise en forme et limitation de l'affichage de l'historique

--- src/app/providers/colleague.service.ts
+++ src/app/providers/colleague.service.ts
@@ -8,17 +8,17 @@ export class ColleagueService {
 
   colleagueList:Colleague[] = [
     {
-      pseudo : "JeanC",
+      pseudo : "JC",
       score : 999,
       photo : "https://file1.telestar.fr/var/telestar/storage/images/media/images/2015/photos/20150403-camera-cafe-que/yvan-le-bolloc-h/817654-3-fre-FR/Yvan-Le-Bolloc-h.jpg?alias=width400&size=x100&format=jpeg"
     },
     {
-      pseudo : "JClaude",
+      pseudo : "JeanC",
       score : -999,
       photo : "https://file1.telestar.fr/var/telestar/storage/images/media/images/2015/photos/20150403-camera-cafe-que/yvan-le-bolloc-h/817654-3-fre-FR/Yvan-Le-Bolloc-h.jpg?alias=width400&size=x100&format=jpeg"
     },
     {
-      pseudo : "JC",
+      pseudo : "JClaude",
       score : 999,
       photo : "https://file1.telestar.fr/var/telestar/storage/images/media/images/2015/photos/20150403-camera-cafe-que/yvan-le-bolloc-h/817654-3-fre-FR/Yvan-Le-Bolloc-h.jpg?alias=width400&size=x100&format=jpeg"
     },
